e3c2811beed2559856f72836be8ad56657a95d7d
hyerimim <hyerim932@naver.com>
2026-07-17 23:30:08 +0900

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/** 바람길 제주 환경 설정 */
-window.BARAMGIL_CONFIG = Object.freeze({
-  NAVER_MAP_KEY_ID: "YOUR_NCP_MAP_KEY_ID",
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxcExx06rEZ8MtB0YtOSy7mK8I8YDo_HcaQ03cYacJfykoDQCaxYfUebyH2fPBMdYGeGQ/exec",
-  DEMO_MODE: true,
-  APP_NAME: "바람길 제주",
-});
